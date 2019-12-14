fd577c085f32db77042b736c7f40d094372bcd32
Madalin Ionescu <madalinu_2006@yahoo.com>
2019-12-15 00:17:32 +0100

    Functions descr

--- GameMaster.h
+++ GameMaster.h
@@ -17,20 +17,27 @@ private:
     unsigned colors;
     unsigned positions;
     unsigned* solution;
-
+    
+    // Fills a given position when generating the prefix. Analoguous to the player
+    // function used to generate a guess.
+    // - v: pointer to working Vector in which to compute the prefix
+    // - vv: pointer to a Vector a Vectors saving the prefixes generated
+    // - position: position to fill in the working vector
+    // - length: length of the sequence to generate
+    // - colors: the number of colors to use
     void cartProduct(sg* v, vg* vv, int position, int length, int colors);
 
 public:
     GameMaster() = default;
     GameMaster(unsigned colors, unsigned positions);
 
-    // returns an array of length 2: 0 -> # perfect colors, 1 -> # correct colors
+    // Returns an array of length 2: 0 -> # perfect colors, 1 -> # correct colors
     sg checkProposedSol(sg proposedSolution);
 
-	// bool victory(std::vector<unsigned>);
-
+    // Prints the solution on the standard output stream
     void printSolution();
 
+    // Returns a pointer to the solution.
     unsigned* getSolution() const;
 
     vg generateFirstPositions(unsigned short posNb);
